d9417ab6eacddb32bd1df29b1d30ee3d196cf950
Kirill <vologdinkirill48@gmail.com>
2026-03-24 10:15:40 +0600

    Centralized build output directory to main premake script

--- premake5.lua
+++ premake5.lua
@@ -12,8 +12,8 @@ project "ImGui"
 
 	warnings "Off"
 
-	targetdir ("%{wks.location}/bin/%{outputdir}/%{prj.name}")
-	objdir ("%{wks.location}/bin-int/%{outputdir}/%{prj.name}")
+	targetdir ("%{outputdir}/%{prj.name}")
+	objdir ("%{intermediatesdir}/%{prj.name}")
 
 	files
 	{
